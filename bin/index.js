618e076df6e0b5f73bd92cba9d3a2efa05d64554
Zack Harley <zack@tenthousandcoffees.com>
2019-01-21 21:17:59 -0500

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+const readline = require('readline');
+const argv = require('yargs').argv;
+
+const Schema = mongoose.Schema;
+
+let db;
+let Idea;
+const ideaSchema = new Schema({
+  idea: String,
+  author: { type: String, default: 'picklehead Jackson' },
+  date: { type: Date, default: Date.now },
+});
+let rl;
+
+(async function() {
+  await mongoose.connect(
+    'mongodb://jumbert:jumbert1@ds141401.mlab.com:41401/idea-jar',
+    { useNewUrlParser: true }
+  );
+  db = mongoose.connection;
+  Idea = mongoose.model('Idea', ideaSchema);
+  main();
+})();
+
+async function main() {
+  const command = argv._[0];
+  if (command === 'ls') {
+    await checkoutIdeas();
+    process.exit(0);
+  } else if (command === 'add') {
+    const idea = argv.m || argv.message;
+    if (!idea) {
+      console.log(chalk.bold.red('Give me an idea you horseradish man, you u'));
+      process.exit(1);
+    }
+    await addIdea(idea);
+    process.exit(0);
+  } else if (command === 'repl') {
+    repl();
+  } else {
+    console.log(chalk.bold.red('Eat shit motherfucker'));
+    process.exit(1);
+  }
+}
+
+async function repl() {
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(
+    chalk.bold.cyan('Do you want to add jims (A) or look at jims (L)? '),
+    async answer => {
+      if (answer.toLowerCase() === 'a') {
+        rl.question(
+          chalk.bold.cyan('What idea would you like to add? '),
+          async idea => {
+            await addIdea(idea);
+            rl.close();
+            process.exit(0);
+          }
+        );
+      } else if (answer.toLowerCase() === 'l') {
+        await checkoutIdeas();
+        rl.close();
+        process.exit(0);
+      } else {
+        console.log(
+          chalk.bold.magenta('you mother fucker pick one of the options')
+        );
+        rl.close();
+        process.exit(1);
+      }
+    }
+  );
+}
+
+async function addIdea(idea) {
+  await Idea.create({ idea });
+  console.log(chalk.green('Idea added successfully:'), idea);
+}
+
+async function checkoutIdeas() {
+  const ideas = await Idea.find({});
+  console.log('');
+  ideas.forEach(idea => console.log(chalk.yellow(`- ${idea.idea}`)));
+}
